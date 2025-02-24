45f18a4891c93c4da48449f1eb362f12b3da8a5e
spectralcode <51879191+spectralcode@users.noreply.github.com>
2025-02-24 02:29:47 +0100

    cleanup

--- src/dispersionestimationengine.h
+++ src/dispersionestimationengine.h
@@ -11,23 +11,6 @@
 #include "octprocessor/processorcontroller.h"
 #include "ascanmetriccalculator.h"
 
-//todo: estimator engine implementieren
-//[x] octprocessor hinzufügen
-//[x] methode für autoloading von oct params erstellen
-//[x] frame signal fangen und roi anwenden [x]
-//[x]oct prozessierung starten und signal mit ergebnis emitten (achtung truncate noch hinzufügen [x])
-//[x]metriccalculator klasse erstellen die dann ergebnis von oct prozessierung bekommt
-//metriccalculator ergebnis wird per signal inkl verwendete werte für d2 und d3 weitergegeben, bools zeigen an was altered wurde
-//ergebnisse plotten
-//status label in gui updaten
-//alle gui connects für parameter update setzen
-//alle nicht benötigten funktionen löschen
-//sobald kompletter durchgang ferttig parabelfit durchführen und plotten und d2 und d3 übertragen und anzeigen
-//evtl. noch einen 1d ascan plot hinzufügen damit mann sieht wie man threshold wert setzen sollte
-//wichtig: load resampling curve from file in cpu processor hinzufügen!
-//extension auto loading implementieren
-//kineo stunden eintragen: 19.01.2025 13:00 bis 19:00
-
 
 class DispersionEstimationEngine : public QObject
 {
